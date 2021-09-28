c36d4c6434a82afc171224865fff0438eeefb2c8
Thiago Mendes Coscrato <thiago.mendes-e@hotmail.com>
2021-09-28 13:31:47 -0300

    Aluraflix v2

---
+++ Aluraflix - Aprendendo Array e Listas/Aluraflix v2/script.js
@@ -0,0 +1,16 @@
+function adicionarFilme() {
+  var filmeFavorito = document.getElementById("filme").value;
+  if (filmeFavorito.endsWith(".jpg")) {
+    listarFilmesNaTela(filmeFavorito);
+  } else {
+    window.alert("Endereço de imagem invalida");
+  }
+  document.getElementById("filme").value = ""; // remove o historico da caixa de texto
+}
+
+function listarFilmesNaTela(filmeFavorito) {
+  var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">";
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML =
+    elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+}
